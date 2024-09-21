aecea6ddd248cd6639d3ecddf814c10c56ade306
nyjames <nsjames@my.waketech.edu>
2024-09-20 23:41:54 -0400

    Created basic login system flask application, Updated README.md
    1. created a base flask app so alongside adding to the app.config section and changing some of the variables for account information (username, password, etc) to match the ones in the database.
    
    2. add three barebone templates (home.html), login.html, register.html) ready for design.
    
    3. added .gitignore for pycache, .env, .venv and *.pyc.
    
    4. Added the requirements.txt and is fully updated as of this commit.
    
    5. Added to the README.Md to provide information on the development team, installation instructions and usage instructions.

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # AI-SHopping-Cart
+
+    The AI-Integrated Shopping Management System is a sophisticated software solution designed to enhance the online shopping experience by leveraging artificial intelligence. 
+    
+    It provides users with a comprehensive toolset for managing their shopping activities, improving efficiency, and making more informed purchasing decisions. 
+
+# Table of Contents
+
+    1. The development Team
+
+    2. Installation
+
+        cloning repo
+        installing dependencies
+
+    3. Usage
+
+        How to start application
+
+
+# 1. V1 Contributors:
+
+    Dominic - Scrum Master & General Engineer
+
+    Navy - Front-End Engineer
+
+    Mariam - Product Owner and Primary Database Engineer
+
+    Talon - Back-End Engineer & Secondary Front-End Engineer
+
+    Nya - Back-End Engineer & Database Engineer
+    
+
+# 2. Installation requirements and steps
+
+    To be able to use this project, you first clone the Repo, using terminal. You can do this by entering
+
+
+        ``` Bash
+        'git clone https://github.com/DominicD213/AI-SHopping-Cart.git'
+        ```
+
+    Install Depenencies:
+
+        ``` Bash
+
+        pip install -r requirements.txt
+         
+        ```
+
+# 3. Usage
+
+    To start the application, locate your local repo in terminal, and enter 'Flask run' or 'python app.py'.
+
+    ``` Bash
+
+    Flask run
+
+    ```
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask("__name__")
+app.secret_key = os.getenv('SECRET_KEY')
+
+# =========== Database connection ================
+
+app.config["MYSQL_HOST"] = os.getenv('MYSQL_HOST')
+app.config["MYSQL_USER"] = os.getenv('MYSQL_USER')
+app.config["MYSQL_PASSWORD"] = os.getenv('MYSQL_PASSWORD')
+app.config["MYSQL_DB"] = os.getenv('MYSQL_DB')
+
+mysql = MySQL(app)
+
+# =================================================
+
+@app.route("/")
+def home():
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        pwd = request.form['password_hash']
+        cur = mysql.connection.cursor()
+        cur.execute(f"SELECT username, password_hash from User where username = '{username}'")
+        user = cur.fetchone()
+        cur.close()
+
+        if user and pwd == user[1]:
+            session['username'] = user[0]
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='invalid username or password')
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        pwd = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute(f"insert into User (username, password_hash) VALUES ('{username}', '{pwd}')")
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('login'))
+    
+    render_template('register.html')
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
