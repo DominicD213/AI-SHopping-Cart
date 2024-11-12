a9873d9450a5ee66e2481ee7dfbf168f7635c68f
Navya Bade <navya.bade@gmail.com>
2024-11-11 21:06:25 -0500

    Added Products to the home page
    - done products page
    - removed double header issues in the cart page

--- React-UI/src/pages/ProductList.jsx
+++ React-UI/src/pages/ProductList.jsx
@@ -1,9 +1,7 @@
 import styled from "styled-components";
-import Navbar from "../components/Navbar";
-import Announcement from "../components/Announcement";
-import Products from "../components/Products";
-import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
 import { mobile } from "../responsive";
 
 const Container = styled.div``;
@@ -39,8 +37,6 @@ const Option = styled.option``;
 const ProductList = () => {
   return (
     <Container>
-      <Navbar />
-      <Announcement />
       <Title>Dresses</Title>
       <FilterContainer>
         <Filter>
